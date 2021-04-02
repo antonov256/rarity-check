47a6d939b8fa2e8c2f0b73afa90e0e80610d4a83
Георгий Антонов <gogiant1992@gmail.com>
2021-04-02 19:25:08 +0300

    - Old simple services

--- src/main/java/com/atriviss/raritycheck/service/OwnItemService.java
+++
@@ -1,7 +0,0 @@
-package com.atriviss.raritycheck.service;
-
-import com.atriviss.raritycheck.model.OwnItem;
-
-public interface OwnItemService {
-    OwnItem findById(Integer id);
-}
--- src/main/java/com/atriviss/raritycheck/service/PortfolioService.java
+++
@@ -1,7 +0,0 @@
-package com.atriviss.raritycheck.service;
-
-import com.atriviss.raritycheck.model.Portfolio;
-
-public interface PortfolioService {
-    Portfolio findByUserId(Integer userId);
-}
