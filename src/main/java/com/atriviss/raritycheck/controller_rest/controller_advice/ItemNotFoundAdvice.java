5d0b6734ddc1123a397feec54a4ef29f09674811
Георгий Антонов <gogiant1992@gmail.com>
2021-04-06 02:26:40 +0300

    Little fixes and improvements
    controller_advice:
    + @ControllerAdvice
    
    ItemModelAssembler:
    ~ RepresentationModelAssembler -> SimpleRepresentationModelAssembler
    
    application.properties:
    + server.port

--- src/main/java/com/atriviss/raritycheck/controller_rest/controller_advice/ItemNotFoundAdvice.java
+++ src/main/java/com/atriviss/raritycheck/controller_rest/controller_advice/ItemNotFoundAdvice.java
@@ -2,10 +2,12 @@ package com.atriviss.raritycheck.controller_rest.controller_advice;
 
 import com.atriviss.raritycheck.controller_rest.exception.ItemNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class ItemNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(ItemNotFoundException.class)
--- src/main/java/com/atriviss/raritycheck/controller_rest/model_assembler/ItemModelAssembler.java
+++ src/main/java/com/atriviss/raritycheck/controller_rest/model_assembler/ItemModelAssembler.java
@@ -2,19 +2,27 @@ package com.atriviss.raritycheck.controller_rest.model_assembler;
 
 import com.atriviss.raritycheck.controller_rest.ItemRestController;
 import com.atriviss.raritycheck.dto_api.ItemApiDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class ItemModelAssembler implements RepresentationModelAssembler<ItemApiDto, EntityModel<ItemApiDto>> {
+public class ItemModelAssembler implements SimpleRepresentationModelAssembler<ItemApiDto> {
     @Override
-    public EntityModel<ItemApiDto> toModel(ItemApiDto apiDto) {
-        return EntityModel.of(apiDto,
-                linkTo(methodOn(ItemRestController.class).one(apiDto.getId())).withSelfRel(),
-                linkTo(methodOn(ItemRestController.class).all()).withRel("items"));
+    public void addLinks(EntityModel<ItemApiDto> resource) {
+        if (resource.getContent() != null)
+            resource.add(linkTo(methodOn(ItemRestController.class).one(resource.getContent().getId())).withSelfRel());
+
+        resource.add(linkTo(methodOn(ItemRestController.class).all(Pageable.unpaged())).withRel("items"));
+    }
+
+    @Override
+    public void addLinks(CollectionModel<EntityModel<ItemApiDto>> resources) {
+        resources.add(linkTo(methodOn(ItemRestController.class).all(Pageable.unpaged())).withRel("items"));
     }
 }
