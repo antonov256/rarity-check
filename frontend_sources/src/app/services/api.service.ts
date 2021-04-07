7c835a0875b95ab77fdf93aadf4f703fc538702f
Nikita Schastnyy <smynay@yandex.ru>
2021-04-07 15:33:00 +0300

    Remove useless routes

--- frontend_sources/src/app/services/api.service.ts
+++ frontend_sources/src/app/services/api.service.ts
@@ -1,4 +1,3 @@
-import { POST_USER_REGISTER } from './../../environments/api.routes';
 import { IAuthResponse } from './../models/http/IAuthResponse';
 import { HttpRequestsService } from './http-requests.service';
 import { Injectable } from '@angular/core';
@@ -11,6 +10,7 @@ export class ApiService {
   constructor(private httpService : HttpRequestsService){}
 
   userRegister(email: string, password: string): Promise<IAuthResponse> {
-    return this.httpService.post<IAuthResponse>(POST_USER_REGISTER, { email, password });
+    // TODO: add route
+    return this.httpService.post<IAuthResponse>('', { email, password });
   }
 }
--- frontend_sources/src/environments/api.routes.ts
+++ frontend_sources/src/environments/api.routes.ts
@@ -1,28 +1,4 @@
 const API_ROUTE = '/api'
 
-export const POST_USER_REGISTER = API_ROUTE + '/user/register';
-export const POST_USER_LOGIN = API_ROUTE + '/user/login';
-export const POST_ITEMS = API_ROUTE + '/items';
-export const POST_CATEGORY = API_ROUTE + '/category';
-
-export const GET_ITEMS_SEARCH = API_ROUTE + '/items/search';
-export const GET_ITEMS = API_ROUTE + '/items';
-export const GET_CATEGORIES = API_ROUTE + '/categories';
-export const GET_CATEGORY = API_ROUTE + '/category';
-export const GET_USER = API_ROUTE + '/user';
-export const GET_USER_PORTFOLIO_WISH_LIST = API_ROUTE + '/portfolio/wishList';
-export const GET_USER_PORTFOLIO_OWN_LIST = API_ROUTE + '/portfolio/ownList';
-export const GET_PROFILE = API_ROUTE + '/profile';
-export const GET_PROFILE_PORTFOLIO_WISH_LIST = API_ROUTE + '/profile/portfolio/wishList';
-export const GET_PROFILE_PORTFOLIO_OWN_LIST = API_ROUTE + '/profile/portfolio/ownList';
-
-export const PATCH_PROFILE_PASSWORD = API_ROUTE + '/profile/password';
-export const PATCH_PROFILE_INFO = API_ROUTE + '/profile/info';
-export const PATCH_ITEMS = API_ROUTE + '/items';
-export const PATCH_CATEGORY = API_ROUTE + '/category';
-export const PATCH_PROFILE_PORTFOLIO_WISH_LIST = API_ROUTE + '/profile/portfolio/wishList';
-export const PATCH_PROFILE_PORTFOLIO_OWN_LIST = API_ROUTE + '/profile/portfolio/ownList';
-export const PATCH_USER = API_ROUTE + '/user';
-
-export const DELETE_ITEMS = API_ROUTE + '/user';
-export const DELETE_CATEGORY = API_ROUTE + '/category';
+export const CATEGORIES_ROUTE = API_ROUTE + '/categories';
+export const ITEMS_ROUTE = API_ROUTE + '/items';
