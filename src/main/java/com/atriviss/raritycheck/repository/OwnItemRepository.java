d5545239529c0a80a5d1adbd64de0fb079d00d1c
Георгий Антонов <gogiant1992@gmail.com>
2021-04-26 20:44:32 +0300

    repositories: added methods for services

--- src/main/java/com/atriviss/raritycheck/repository/OwnItemRepository.java
+++ src/main/java/com/atriviss/raritycheck/repository/OwnItemRepository.java
@@ -4,7 +4,9 @@ import com.atriviss.raritycheck.dto_jpa.pc_app.OwnItemJpaDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OwnItemRepository extends JpaRepository<OwnItemJpaDto, Integer> {
-
+    List<OwnItemJpaDto> findAllByUserId(Integer userId);
 }
--- src/main/java/com/atriviss/raritycheck/repository/WishItemRepository.java
+++ src/main/java/com/atriviss/raritycheck/repository/WishItemRepository.java
@@ -4,7 +4,9 @@ import com.atriviss.raritycheck.dto_jpa.pc_app.WishItemJpaDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishItemRepository extends JpaRepository<WishItemJpaDto, Integer> {
-
+    List<WishItemJpaDto> findAllByUserId(Integer userId);
 }
