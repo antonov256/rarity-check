f3e49ca1357573efcf37a6433e3a89208e6c7a5e
Георгий Антонов <gogiant1992@gmail.com>
2021-04-29 12:51:18 +0300

    ResourceNotFoundException improvement

--- src/main/java/com/atriviss/raritycheck/controller_rest/exception/ResourceNotFoundException.java
+++ src/main/java/com/atriviss/raritycheck/controller_rest/exception/ResourceNotFoundException.java
@@ -4,6 +4,9 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resourceName, Number resourceId) {
         super("Could not find " + resourceName + " with id=" + resourceId);
     }
+    public ResourceNotFoundException(Class<?> resourceClass, Number resourceId) {
+        this(resourceClass.getSimpleName(), resourceId);
+    }
 
     public ResourceNotFoundException(String message) {
         super(message);
